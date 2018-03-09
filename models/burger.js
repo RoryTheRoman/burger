8b014c050e40f0ae6b9d6f5171e503e5847ba4ed
Mike D <michael.mcdaniel210@gmail.com>
2018-03-08 22:24:18 -0600

    added delete buttons and functionality

--- models/burger.js
+++ models/burger.js
@@ -16,6 +16,12 @@ var burger = {
             cb(res);
         });
     },
+    deleteOne: function (condition, cb){
+        orm.deleteOne("burgers", condition, function (res) {
+            cb(res);
+        });
+    }
+    
 
 }
 
