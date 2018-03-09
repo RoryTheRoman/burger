c61e23253bbd16927792f2b8ee7a6b7272dea2eb
Mike D <michael.mcdaniel210@gmail.com>
2018-03-08 20:11:35 -0600

    displaying to page now.  devour buttons created.

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,61 @@
 var connection = require("./connection.js")
+var tableInput = "burgers";
+function printQuestionMarks(num) {
+    var arr = [];
 
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    var arr = [];
+
+
+    for (var key in ob) {
+        var value = ob[key];
+
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+
+    return arr.toString();
+}
 var orm = {
-    selectAll: function(){
-
+    selectAll: function(tableInput, callback){
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        });
     },
-    insertOne: function(){
+    insertOne: function(table, cols, vals, cb){
+        var queryString = "INSERT INTO " + table;
 
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
     },
     updateOne(){
         
